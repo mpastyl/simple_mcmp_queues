60502ee148088722fc0042d00fae99db0d78e0b8
Charalampos Stylianopoulos <mpastyl12@gmail.com>
2018-01-27 21:59:54 +0100

    Switched to pthreads and pinned on cpu

--- test_lock_based.c
+++ test_lock_based.c
@@ -1,24 +1,39 @@
+#define _GNU_SOURCE
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdint.h>
 #include<sys/time.h>
 #include<unistd.h>
+#include<pthread.h>
+#include<sched.h>
 #include "mcmp_bounded_lock_based.h"
-
 //benchmark configurables
-
-uint32_t no_threads=4;
+uint32_t no_threads=16;
 uint32_t size = 1024*16;
 int bench_duration_sec = 20;
 //
 
 //globals
-int stop=0;
+volatile int stop=0;
+volatile int barrier=0;
 //
 
+void setaffinity_oncpu(unsigned int cpu)
+{
+    cpu_set_t cpu_mask;
+    int err;
 
+    CPU_ZERO(&cpu_mask);
+    CPU_SET(cpu, &cpu_mask);
+
+    err = sched_setaffinity(0, sizeof(cpu_set_t), &cpu_mask);
+    if (err) {
+        perror("sched_setaffinity");
+        exit(1);
+    }
+}
 typedef struct thread_data_struct{
-		
+		int tid;
 		struct timeval start;
 		struct timeval end;
 		int succ_enq;
@@ -29,8 +44,13 @@ typedef struct thread_data_struct{
 struct thread_data_struct * thread_data;
 
 
-void thread_loop(int tid){
+void thread_loop(void * tid_void_ptr){
 
+
+
+	int tid = (int) tid_void_ptr;
+	setaffinity_oncpu(tid%14);
+	while(!barrier) {};
 	struct thread_data_struct * my_data = & thread_data[tid];
 	gettimeofday(&my_data->start, NULL);
 	int succ_enq=0;
@@ -51,8 +71,6 @@ void thread_loop(int tid){
 				succ_deq++;
 			}
 		}
-
-
 	}
 	gettimeofday(&my_data->end, NULL);
 	my_data->succ_enq =  succ_enq;
@@ -61,7 +79,6 @@ void thread_loop(int tid){
 }
 
 int main(){
-
 
 
 	PRINTF(" calling init_queue\n");
@@ -90,15 +107,32 @@ int main(){
 	//------  BENCHMARK -------
 	
 	srand(0);
-	
 	thread_data = (struct thread_data_struct *) malloc(no_threads * sizeof(thread_data_struct));
 	for(i=0;i<no_threads;i++){
+		thread_data->tid=0;
 		thread_data->succ_enq=0;
 		thread_data->succ_deq=0;
 		thread_data->data_dump=0;
 	}
 	
+	
+	stop=0;
+	barrier=0;
+	pthread_t threads[no_threads];	
+	for (i=0;i<no_threads;i++){
+		thread_data->tid=i;
+		int rc = pthread_create(&threads[i], NULL, thread_loop, (void *) thread_data->tid);
+		if (rc){
+			printf(" Could not launch all threads, exiting....\n");
+			return 0;
+		}
+	}
+
 	printf("Starting benchmark \n");
+	barrier=1;
+	sleep(bench_duration_sec);
+	stop=1;
+	/*
 	#pragma omp parallel num_threads(no_threads+1) 
 	{	
 		// The extra thread sleeps and signals everyone to stop
@@ -111,8 +145,11 @@ int main(){
 		}
 
 	}
+	*/
 	printf("Benchmark finished\n");
-
+	for(i=0; i<no_threads;i++){
+		pthread_join(threads[i],NULL);
+	}
 	long int total_succ_ops = 0;
 	int data_dump=0;
 	for(i=0; i<no_threads;i++){
